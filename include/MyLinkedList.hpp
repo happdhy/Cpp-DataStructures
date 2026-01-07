6aae4bb81a257ea15435c35ebd7c6665b31d31b8
lioner <2671682581@qq.com>
2026-01-07 21:39:41 +0800

    updata-list-2

--- include/MyLinkedList.hpp
+++ include/MyLinkedList.hpp
@@ -3,6 +3,8 @@
 
 #include<iostream>
 
+using namespace std;
+
 template<typename T>
 struct Node{
     T data;
@@ -17,7 +19,10 @@ private:
     int size;
 
 public:
-    MyLinkedList(): head(nullptr), size(0){}
+    MyLinkedList(){
+        auto node = new Node<T>(0);
+        head = node;
+    }
 
     ~MyLinkedList(){
         auto current_node = head;
@@ -38,6 +43,67 @@ public:
     int GetSize(){
         return this->size;
     }
+
+    void push_front(const T& val){
+        Node<T>* new_node  = new Node<T>(val);
+        new_node->next = head->next;
+        head->next = new_node;
+        size++;
+    }
+
+    void push_back(const  T& val){
+        Node<T>* cur_node = head;
+        while(cur_node->next != nullptr) cur_node = cur_node->next;
+        Node<T>* new_node = new Node<T>(val);
+        cur_node->next = new_node;
+    }
+
+    void pop_front(){
+        if(head->next){
+            Node<int>* del_node = head->next;
+            head->next = head->next->next;
+            delete del_node;
+        }
+    }
+
+    void pop_back(){
+        Node<T>* p = head, q = p->next;
+        if(head->next)
+        {
+            while(q->next!=nullptr)
+            {
+                p = q;
+                q = q->next;
+            }
+            p->next = nullptr;
+            delete q;
+        }
+    }
+
+    bool find(const T& val){
+        Node<T>* node = head->next;
+        while(node)
+        {
+            if(node->data==val) return true;
+            node = node->next;
+        }
+    }
+
+    void print(){
+        Node<T>* cur_node = head->next;
+        if(cur_node == nullptr) cout<<"NULL";
+        else
+        {
+            while(cur_node->next)
+            {
+                print<<cur_node->data<<" -> ";
+                cur_node = cur_node->next;
+            }
+            cout<<"NULL";
+        }
+    }
+    
+
 
 };
 
